19a6ac098a4553d52d5c523248564d631f471415
s3-viof <viktor.ofjall@sector3studios.com>
2016-10-04 14:04:53 +0200

    Added trimming when printing player name

--- sample-csharp/src/Program.cs
+++ sample-csharp/src/Program.cs
@@ -110,6 +110,7 @@ namespace R3E
         {
             if (Read())
             {
+                Console.WriteLine("Name: {0}", System.Text.Encoding.UTF8.GetString(_data.PlayerName).TrimEnd('\0'));
 
                 if (_data.Gear >= -1)
                 {
